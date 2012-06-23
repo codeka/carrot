65604a52bd5138ee8023ed1cf9998de997ac66b0
anysome@gmail.com <anysome@gmail.com@0e552f88-7a8f-11de-888d-2103c0e6b660>
2012-06-23 14:32:42 +0000

    git-svn-id: http://jangod.googlecode.com/svn/trunk@97 0e552f88-7a8f-11de-888d-2103c0e6b660

--- site/upload.cmd
+++ site/upload.cmd
@@ -1,4 +1,4 @@
-set GAE=D:\ide\eclipse\plugins\com.google.appengine.eclipse.sdkbundle.1.3.1_1.3.1.v201002101412\appengine-java-sdk-1.3.1\bin\
+set GAE=D:\IDE\eclipse\plugins\com.google.appengine.eclipse.sdkbundle_1.6.6\appengine-java-sdk-1.6.6\bin\
 java -cp ../bin/ WebGenerator
 call "%GAE%appcfg.cmd" update war
 pause
