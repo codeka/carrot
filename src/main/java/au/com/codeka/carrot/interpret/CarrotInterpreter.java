622a4f1ea94525ebaade8002155ab2bdf2a2fae3
Dean Harding <dean@codeka.com.au>
2017-02-21 06:49:57 +1100

    Removed unneeded file.

--- src/main/java/au/com/codeka/carrot/interpret/CarrotInterpreter.java
+++
@@ -1,7 +0,0 @@
-package au.com.codeka.carrot.interpret;
-
-/**
- * Main interpreter which interprets a parsed template and writes the results to an output {@link Writer}.
- */
-public class CarrotInterpreter {
-}
